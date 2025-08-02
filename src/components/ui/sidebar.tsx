5195c0085d935aa1df548bda2560d44021a5e76f
HORNADA, Rocelle Ann <rocelleann.hornada@ul.edu.ph>
2025-08-02 04:17:30 +0000

    the side bar is getting on top of the main screen so i cannot see

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -322,7 +322,9 @@ const SidebarInset = React.forwardRef<
     <main
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-background",
+        "relative flex min-h-svh flex-1 flex-col bg-background transition-[margin-left] duration-200 ease-linear",
+        "peer-data-[state=expanded]:md:ml-[var(--sidebar-width)]",
+        "peer-data-[state=collapsed]:peer-data-[collapsible=icon]:md:ml-[var(--sidebar-width-icon)]",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
@@ -761,3 +763,5 @@ export {
   SidebarTrigger,
   useSidebar,
 }
+
+    
